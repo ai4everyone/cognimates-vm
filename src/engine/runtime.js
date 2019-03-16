873627226509d38514fa7df16c6e0200f672f4a7
satvu <sarahvu@mit.edu>
2019-03-16 11:23:21 -0400

    load extensions in menu at start

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -36,7 +36,6 @@ const defaultBlockPackages = {
     scratch3_sound: require('../blocks/scratch3_sound'),
     scratch3_sensing: require('../blocks/scratch3_sensing'),
     scratch3_data: require('../blocks/scratch3_data'),
-    scratch3_procedures: require('../blocks/scratch3_procedures')
 };
 
 /**
